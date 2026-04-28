3bfe900a95ca55595f35f722582078e0c7b7d4b1
kaiomarian <161875538+kaiomarian@users.noreply.github.com>
2026-04-28 09:53:48 -0300

    Create Aulateste2 class with basic data types
    Add Java class Aulateste2 with variable declarations and comments.

---
+++ anotações de java.java
@@ -0,0 +1,91 @@
+public class Aulateste2 {
+    byte estado = 52;
+    short meuinteiro = 2500;
+    int peso = 70;
+    long numeroastronomico = 23434234234234;
+    float altura = 1.70f;
+    double pi = 3.14159265358;
+    boolean aberto = true;
+    char letra = 'j';
+    String nome = 'josivan';
+}
+// comentario de linha unica
+/* comentario 
+   de multiplas linhas */
+/** comentario de 
+ * documentação */
+
+/* operações:
+
++ adição
+- subtração
+* multiplicação
+/divisão
+-a subtração unária
++a adição unária
+// resto da divisão
+
+lógicas:
+
+== igual
+!= diferença
+> maior que
+< menor que 
+>= ou <= maior/menor ou igual que
+
+
+
+PILHAS:
+import java.util.Stack
+
+construir:
+Stack<integer> pilha = new Stack();
+
+adcionar:
+pilha.push(numero);
+
+remover:
+int x = pilha.pop(); (apaga por numero, fila.pop(x) apaga por indice.)
+
+examinar:
+int x = pilha.peek();
+
+esvasiar
+pilha.clear();
+
+tamanho
+pilha.size();
+
+
+
+FILAS:
+import java.util.LinkedList
+import java.util.Queue
+
+construir:
+Queue<integer> fila = new LinkedList();
+
+adcionar:
+lista.add(numero);
+
+remover:
+int x = lista.remove(); (apaga por numero, lista.remove(x) apaga por indice.)
+
+examinar:
+int x = lista.element();
+
+esvaziar:
+lista.clear;
+
+tamanho:
+lista.size;
+
+recuperar elemento:
+lista.get(indice);
+
+pesquisar por elemento:
+int pos = lista.indexOf(numero); (retorna a primeira instancia do elemento. caso não exista, retorna -1)
+
+ordenar:
+Collection.sort(lista);
+*/
